8340ae5ce2d1f5fdd08cc5e25356ade350513b01
Marcin Brzozowski <brzozowski593@gmail.com>
2023-11-26 20:18:31 +0100

    Setup base design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'bellefair':['Bellefair', 'serif']
+      }
+    },
   },
   plugins: [],
 }
