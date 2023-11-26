821c391aae476b0ad4117d8d986f49026ddc3930
Marcin Brzozowski <brzozowski593@gmail.com>
2023-11-26 21:19:46 +0100

    Create Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Footer from "./components/Footer.vue";
+</script>
 
 <template>
   <router-view v-slot="{ Component }">
     <component :is="Component" />
   </router-view>
+  <Footer />
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sponsor-block">
+    <h1>tutaj pojawia się zdjecia sponsorów</h1>
+  </div>
+</template>
+
+<style scoped>
+.sponsor-block {
+  position: absolute;
+  bottom: 0;
+  background-color: white;
+  height: 15%;
+  width: 100%;
+}
+</style>
