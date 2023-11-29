5b95544001c3e82474d50490031d308a1d49c142
Marcin Brzozowski <brzozowski593@gmail.com>
2023-11-30 00:19:10 +0100

    Add a simple transition during the view switch

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,23 @@ import Footer from "./components/Footer.vue";
 
 <template>
   <router-view v-slot="{ Component }">
-    <component :is="Component" />
+    <transition name="scale" mode="out-in">
+      <component :is="Component" />
+    </transition>
   </router-view>
 
-  <DevNav />
+  <DevNav/>
 </template>
+
+<style scoped lang="scss">
+.scale-enter-active,
+.scale-leave-active {
+  transition: all 0.5s ease;
+}
+
+.scale-enter-from,
+.scale-leave-to {
+  opacity: 0;
+  transform: scale(0.9);
+}
+</style>
